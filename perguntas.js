0891f9bceeb6969db9f46670e6d4149b42f56051
eduardatag05 <eduarda.tagnon@escola.pr.gov.br>
2025-06-17 23:27:46 +0000

    nddtj

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,69 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'celular',
+    'Qual a diferença entre 5G e 4G?',
+    ' O 5G é mais rápido, tem menos atraso e suporta mais dispositivos conectados ao mesmo tempo que o 4G.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'celular',
+    ' O que significa "tela de 120Hz"?' 
+    'É a quantidade de vezes que a tela atualiza a imagem por segundo. 120Hz é mais fluido que os tradicionais 60Hz, melhorando jogos e rolagem de tela.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'celular',
+    'Por que alguns celulares esquentam muito?',
+    'Pode ser por uso intenso (jogos, vídeos), processador fraco, bateria ruim ou ambiente quente.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'celular',
+    ' O que é carregamento rápido?',
+    'Tecnologia que recarrega a bateria mais rápido que o normal (ex.: 30W, 65W). Alguns celulares carregam 50% em 15 minutos.'
+)
+criaCartao(
+    'celular',
+    'Para que serve a câmera "ultrawide"?',
+    'Ela tira fotos mais abertas, capturando mais cenário que a câmera normal (boa para paisagens e grupos).'
+)
+
+criaCartao(
+    'celular',
+    'O que é resistência IP68?',
+    'Significa que o celular é à prova d’água (mergulho até 1,5m por 30min) e poeira.'
+)
+
+criaCartao(
+    'celular',
+    'Por que alguns celulares não têm entrada para fone?',
+    'Muitas marcas removem para deixar o celular mais fino ou incentivar o uso de fones bluetooth.'
+)
+
+criaCartao(
+    'celular',
+    'O que é "modo noturno" na câmera?',
+    ' Melhora fotos no escuro, deixando a imagem mais clara e com menos borrão.'
+)
+criaCartao(
+    'celular',
+    'Qual a vantagem de ter vários chips (dual chip)?',
+    'Usar dois números (pessoal e trabalho) ou operadoras diferentes no mesmo celular.'
+)
+
+criaCartao(
+    'celular',
+    'O que é "desbloqueio por reconhecimento facial"?',
+    'O celular abre só ao reconhecer seu rosto (como no iPhone ou Android).'
+)
+
+criaCartao(
+    'celular',
+    ' Por que a bateria dura menos com o tempo?',
+    'Baterias se desgastam após muitos ciclos de carga. Em 2-3 anos, a capacidade pode cair pela metade.'
+)
+
+criaCartao(
+    'celular',
+    'O que é um "celular dobrável"?',
+    'Tem tela flexível que dobra, virando tablet pequeno quando aberto (ex.: Samsung Galaxy Z Fold).'
 )
